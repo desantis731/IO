8903955862db9b08d3f169f1fd11f3a1de5d751d
desantis731 <77997111+desantis731@users.noreply.github.com>
2022-11-22 09:25:07 -0500

    Create runBlackDuckScan.sh

---
+++ runBlackDuckScan.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo 'Running Black Duck'
+wget -O detect.sh https://detect.synopsys.com/detect.sh
+
+chmod a+x detect.sh
+./detect.sh --blackduck.url=${Blackduck_InstanceUrl} --blackduck.api.token=${Blackduck_AuthToken} --detect.project.name=${Blackduck_ProjectName} --detect.project.version.name=${Blackduck_ProjectVersion} --detect.force.success=true
