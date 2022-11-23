585d8a6851fbcbfd4e587ecbc7c0188a9bf986b0
desantis731 <77997111+desantis731@users.noreply.github.com>
2022-11-23 13:16:17 -0500

    Update runBlackDuckScan.sh

--- runBlackDuckScan.sh
+++ runBlackDuckScan.sh
@@ -4,4 +4,4 @@ echo 'Running Black Duck'
 wget -O detect.sh https://detect.synopsys.com/detect.sh
 
 chmod a+x detect.sh
-./detect.sh --blackduck.url=${Blackduck_InstanceUrl} --blackduck.api.token=${Blackduck_AuthToken} --detect.project.name=${Blackduck_ProjectName} --detect.project.version.name=${Blackduck_ProjectVersion} --detect.force.success=true
+./detect.sh --blackduck.url=${Blackduck_InstanceUrl} --blackduck.api.token=${Blackduck_AuthToken} --detect.project.name=${Blackduck_ProjectName} --detect.project.version.name=${Blackduck_ProjectVersion} --detect.force.success=true --blackduck.trust.cert=true --logging.level.com.synopsys.integration=DEBUG
